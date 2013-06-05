1fd27d350e9eb26e1960d68e7a85882d53cdc63e
Leon Renkema <leonrenkema@gmail.com>
2013-06-05 16:07:37 +0200

    Add mpdclient

--- index.py
+++ index.py
@@ -4,6 +4,77 @@ import re
 
 from datetime import timedelta
 
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# IMPORTS
+import sys
+import pprint
+
+from mpd import (MPDClient, CommandError)
+from socket import error as SocketError
+
+HOST = 'localhost'
+PORT = '6600'
+PASSWORD = False
+##
+CON_ID = {'host':HOST, 'port':PORT}
+##  
+
+## Some functions
+def mpdConnect(client, con_id):
+    """
+    Simple wrapper to connect MPD.
+    """
+    try:
+        client.connect(**con_id)
+    except SocketError:
+        return False
+    return True
+
+def mpdAuth(client, secret):
+    """
+    Authenticate
+    """
+    try:
+        client.password(secret)
+    except CommandError:
+        return False
+    return True
+##
+
+def getstatus():
+    ## MPD object instance
+    client = MPDClient()
+    if mpdConnect(client, CON_ID):
+        print 'Got connected!'
+    else:
+        print 'fail to connect MPD server.'
+        sys.exit(1)
+
+    # Auth if password is set non False
+    if PASSWORD:
+        if mpdAuth(client, PASSWORD):
+            print 'Pass auth!'
+        else:
+            print 'Error trying to pass auth.'
+            client.disconnect()
+            sys.exit(2)
+
+    ## Fancy output
+    # pp = pprint.PrettyPrinter(indent=4)
+
+    ## Print out MPD stats & disconnect
+    output = client.list('file') #stats()
+
+    # pp.pprint(client.stats())
+
+    client.disconnect()
+    return output
+		#sys.exit(0)
+
+
 #
 # Get root
 #
@@ -68,6 +139,9 @@ def application(environ, start_response):
 				output = "bla"
 			elif request_method == "GET":
 				output = "get device"
+		elif matches["resource"] == "music":
+			if matches["id"] == "files":
+				output = getstatus() 
 
 		elif matches["resource"] == "sensors":
 			if request_method == "POST":
